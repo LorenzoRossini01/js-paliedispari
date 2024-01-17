6c5f9776e02b0ce626a75fc3dd66e257cae9bced
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-01-17 16:26:12 +0100

    feat: add input type select with js function

--- pari o dispari/js/main.js
+++ pari o dispari/js/main.js
@@ -1,6 +1,14 @@
-console.log("js ok");
+const oddEvenSelectInput = document.getElementById("odd-even-select-input");
+const userNumberInput = document.getElementById("user-number-input");
 
-const resultElement = document.getElementById("results");
+const oddEvenArray = ["pari", "dispari"];
+const numberArray = [1, 2, 3, 4, 5];
 
-getRandomNumber(1, 20);
-console.log(randomNumber);
+function addOptionToSelectInput(array, input) {
+  for (let i = 0; i < array.length; i++) {
+    input.innerHTML += `<option value="${array[i]}">${array[i]}</option>`;
+  }
+}
+
+addOptionToSelectInput(oddEvenArray, oddEvenSelectInput);
+addOptionToSelectInput(numberArray, userNumberInput);
